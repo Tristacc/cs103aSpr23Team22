4660445ddf56bc99f6171645d5c3e54d00fcf159
Trista Lu <t09b02307@ntu.edu.tw>
2023-03-24 11:35:45 -0400

    added show, add, and delete operation setting; also added some basic methods including runQuery() and topleve()

---
+++ PA03/tracker.py
@@ -0,0 +1,71 @@
+
+from transactions import Transaction
+import sys
+
+
+def usage():
+    print('''usage:
+            tracker add [item#] [amount] [category] [date] [description]
+            tracker show 
+            tracker delete [item#]
+            
+            '''
+            )
+def showTable(items):
+    if len(items)==0:
+        print('no tasks to print')
+        return
+    print('\n')
+    
+    print("%-10s %-10s %-10s %-6s %-5s"%('item#','amount','category','date','description'))
+    print('-'*70)
+    for item in items:
+        values = tuple(item.values()) 
+        print("%-10d %-10d %-10s %04s %10s"% ( values[1], values[2], values[3], values[4],"  "+values[5]))
+
+def process_args(arglist):
+    ''' examine args and make appropriate calls to TodoList'''
+    tracker = Transaction()
+    if arglist==[] or arglist==["menu"]:
+        usage()
+
+    elif arglist[0]=="show":
+        showTable(tracker.showTransactions())
+
+    elif arglist[0]=='add':
+        if len(arglist)!=6:
+            print(len(arglist))
+            print("more information needed")
+            usage()
+        else:
+            userInput = {'item_num':arglist[1],'amount':arglist[2],'category':arglist[3], 'date':arglist[4],'description':arglist[5]}
+            tracker.add(userInput)
+
+    elif arglist[0] =='delete':
+        if len(arglist)!=2:
+            print("please enter the item#")
+            usage()
+        else:
+            userInput = {'item_num':arglist[1]}
+            tracker.delete(userInput)
+
+    
+
+def toplevel():
+    if len(sys.argv)==1:
+        usage()
+        args = []
+        while args!=['']:
+            args = input("tracker> ").split(' ')
+            if args[0]=='add':
+                args = ['add',args[1],args[2],args[3],args[4]," ".join(args[5:])]
+            if args[0]=='delete':
+                args = ['delete',args[1]]
+            process_args(args)
+            print('-'*70+'\n')
+    else:
+        args = sys.argv[1:]
+        process_args(args)
+        print('-'*70+'\n')
+    
+toplevel()
---
+++ PA03/transactions.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+
+def toDict(t):
+    transaction = {'rowid':t[0], 'item_num':t[1], 'amount':t[2], 'category':t[3], 'date':t[4],'description':t[5]}
+    return transaction
+
+class Transaction():
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions
+                    (item_num int, amount int, category text, date int, description text)''',())
+      
+    def add(self, item):
+        return self.runQuery("INSERT INTO transactions VALUES(?,?,?,?,?)",(item['item_num'],item['amount'],item['category'],item['date'],item['description']))
+     
+    def showTransactions(self):
+        return self.runQuery("SELECT rowid,* from transactions",())
+    
+    def delete(self, item):
+        return self.runQuery("DELETE FROM transactions WHERE item_num=(?)",(item['item_num'],))
+    
+    def runQuery(self,query,tuple):
+        con= sqlite3.connect(os.getenv('HOME')+'/Desktop/cs103aSpr23Team22/PA03/tracker.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
